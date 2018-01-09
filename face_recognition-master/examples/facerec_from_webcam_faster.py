bfdc8be2300ef32d9cfe715e544cb178932cfc28
Bliss <blysxaye@icloud.com>
2018-01-10 01:14:08 +0800

    no queue yet

--- face_recognition-master/examples/facerec_from_webcam_faster.py
+++ face_recognition-master/examples/facerec_from_webcam_faster.py
@@ -1,6 +1,32 @@
 import face_recognition
 import cv2
 import numpy
+import queue as queue
+from threading import Thread
+import time
+import pyttsx3 as tts
+import speech_recognition as sr
+    
+class dotdict(dict):
+# """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+class Ingredient:
+    bread = 1
+    bacon = 2
+    cheese = 3
+    beef = 4
+    candy = 5
+    cake = 6
+    lettuce = 7
+class Burger:
+    hamburger = dotdict({"ingredients":[Ingredient.bread, Ingredient.lettuce, Ingredient.beef, Ingredient.bread], "name":"hamburger", "value":0, "done":False})
+    cheeseburger = dotdict({"ingredients":[Ingredient.bread, Ingredient.lettuce, Ingredient.cheese, Ingredient.beef, Ingredient.bread], "name":"hamburger", "value":1, "done":False})
+    baconburger = dotdict({"ingredients":[Ingredient.bread, Ingredient.bacon, Ingredient.cheese, Ingredient.beef, Ingredient.bread], "name":"hamburger", "value":2, "done":False})
+    sweetburger = dotdict({"ingredients":[Ingredient.bread, Ingredient.candy, Ingredient.cake, Ingredient.bread], "name":"hamburger", "value":3, "done":False})
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
@@ -29,6 +55,100 @@ database_face = [obama_face_encoding]
 #should be 300
 FACE_SIZE = 300
 faceID = 0
+
+
+currentBurger = None
+burgerQuere = []
+# I WILL DO THREAD ROBOT ARM
+# _________________________________
+# Create thread for robot arm
+
+def do_stuff(q):
+    while True:
+        try:
+            if q.get()[0] == 1:
+                do_robot(q)
+            
+            elif q.get()[2] == 1:
+                do_robotserve(q)
+            else:
+                do_bang(q)
+        except:
+            print("BOMB")
+            q.task_done()
+    
+def do_robot(q):
+    global burgerQuere
+    for burger in burgerQuere:
+        if burger.done != 1:
+            burger.done = 1
+            break
+#	while True:
+    print("robot making %s", (q.get()[1].name,))
+    q.task_done()
+    print("robot Done")
+def do_bang(q):
+    print("robot serving %s", (q.get()[1].name,))
+    print("Bang!!")
+    q.task_done()
+
+q = queue.Queue(maxsize=0)
+worker = Thread(target=do_stuff, args=(q,))
+worker.setDaemon(True)
+worker.start()
+
+
+def inputLoop():
+    global currentBurger
+#    print ColorTextExt.PROPMTEXT + "What are you? \n " + "WEREWOLF = 0    SEER = 1    THIEF = 2    VILLAGER = 3 \n " + "VOTE = v[role]    STEAL = t[person][role]    OTHER = p[person][role]" + ColorTextExt.RESET + "\n"
+    while 1:
+        input_string = input()
+        try:
+            i = int(float(input_string))
+            if i == 1:
+                print(Burger.hamburger.name)
+                currentBurger = Burger.hamburger
+            elif i == 2:
+                print(Burger.cheeseburger.name)
+                currentBurger = Burger.cheeseburger
+            elif i == 3:
+                print(Burger.baconburger.name)
+                currentBurger = Burger.baconburger
+            elif i == 4:
+                print(Burger.sweetburger.name)
+                currentBurger = Burger.sweetburger
+        except:
+            print("Input something else")
+        
+
+inputor = Thread(target=inputLoop, args=())
+inputor.setDaemon(True)
+inputor.start()
+
+#engine = tts.init()
+r = sr.Recognizer()
+
+#def looking_at_face():
+#    global video_capture
+#
+#    # Load a sample picture and learn how to recognize it.
+#    global obama_image
+#    global obama_face_encoding
+#
+#    # Initialize some variables
+#    global face_locations
+#    global face_encodings
+#    global face_names
+#    global process_this_frame
+#
+#    global database_name
+#    global database_face
+#
+#    #should be 300
+#    global FACE_SIZE
+#    global faceID
+#    global process_this_frame
+#    
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -48,14 +168,21 @@ while True:
             match = face_recognition.face_distance(database_face, face_encoding)
             name = "Unknown"
 #            print(match)
-            if min(match) <= 0.38: #or 0.40
+            if min(match) <= 0.39: #or 0.40
                 matchIndex = numpy.where(match==min(match)) 
                 name = database_name[matchIndex[0][0]]
-            elif min(match) >= 0.505:
+                if burgerQuere[matchIndex[0][0]-1].done == 1:
+                    q.put((1,matchIndex[0][0]-1))
+            elif min(match) >= 0.53:
 #                new_face_encoding = face_recognition.face_encodings(frame)[0]
-                database_face.append(face_encoding)
-                database_name.append('Person %d' % (faceID,))
-                faceID += 1
+            
+                if currentBurger != None:
+                    database_face.append(face_encoding)
+                    database_name.append('%s Person %d' % (currentBurger.name,faceID,))
+                    faceID += 1
+                    q.put((0,currentBurger))
+                    burgerQuere.append(currentBurger)
+                    currentBurger = None
 #                not in database
 
             face_names.append(name)
@@ -84,12 +211,46 @@ while True:
             #Do checking face size here
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    
+
+        cv2.imshow('Video', frame)
+    
+    
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# Release handle to the webcam
+    
+    # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
+#looker = Thread(target=looking_at_face, args=())
+#looker.setDaemon(True)
+#looker.start()
+
+def listen_hey():
+    print("Say something!")
+#    engine.say("Say Something")
+#    engine.runAndWait()
+    
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+    
+    speech = ""
+    try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+        speech = "Google Speech Recognition thinks you said " + r.recognize_google(audio)
+    except sr.UnknownValueError:
+        speech = "Google Speech Recognition could not understand audio"
+    except sr.RequestError as e:
+        speech = "Could not request results from Google Speech Recognition service; {0}".format(e)
+    
+    print(speech)
+#    engine.say(speech)
+#    engine.runAndWait()
+
+#while 1:
+#    pass
--- speech.py
+++ speech.py
@@ -1,29 +1,39 @@
 import pyttsx3 as tts
 import speech_recognition as sr
+from threading import Thread
+import time
 
 engine = tts.init()
 r = sr.Recognizer()
 
-while(True):
-    print("Say something!")
-    engine.say("Say Something")
-    engine.runAndWait()
-    
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    
-    speech = ""
-    try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-        speech = "Google Speech Recognition thinks you said " + r.recognize_google(audio)
-    except sr.UnknownValueError:
-        speech = "Google Speech Recognition could not understand audio"
-    except sr.RequestError as e:
-        speech = "Could not request results from Google Speech Recognition service; {0}".format(e)
-    
-    print(speech)
-    engine.say(speech)
-    engine.runAndWait()
-    
+def listen_to_music_all_day_everyday():
+    while(True):
+        print("Say something!")
+        engine.say("Say Something")
+        engine.runAndWait()
+        
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+        
+        speech = ""
+        try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+            speech = "Google Speech Recognition thinks you said " + r.recognize_google(audio)
+        except sr.UnknownValueError:
+            speech = "Google Speech Recognition could not understand audio"
+        except sr.RequestError as e:
+            speech = "Could not request results from Google Speech Recognition service; {0}".format(e)
+        
+        print(speech)
+        engine.say(speech)
+        engine.runAndWait()
+
+
+worker = Thread(target=listen_to_music_all_day_everyday, args=())
+worker.setDaemon(True)
+worker.start()
+
+while 1:
+    pass
