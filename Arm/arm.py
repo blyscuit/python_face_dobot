5a0b75e6bb8b159d3b9f769ff95d1a1b9602e1f1
ChollSong <chollsong212@gmail.com>
2018-01-10 20:04:14 +0800

    Bug fixed in arm

--- Arm/arm.py
+++ Arm/arm.py
@@ -1,7 +1,6 @@
 import time
 from glob import glob
-
-from pydobot import Dobots
+from pydobot import Dobot
 
 class dbot:
 
@@ -15,35 +14,44 @@ class dbot:
     tray = [0.0, 250.0, z_release]
 
     def __init__(self):
-        device = Dobot(port=PORT)
+        self.device = Dobot(port = dbot.PORT)
+        self.setSpeed(10)
+        time.sleep(2)
 
     def hide(self):
-        device.go(*hide)
+        self.device.go(*dbot.hide)
         time.sleep(5)
 
     def pickUp(self, x, y):
-        device.go(x, y, z_min)
+        self.device.go(x, y, dbot.z_min)
         time.sleep(5)
-        device.suck(True)
+        self.device.suck(True)
         time.sleep(1)
-        device.go(x, y, 0.0)
+        self.device.go(x, y, 0.0)
         time.sleep(5)
     
-    def place(self)
-        device.go(*tray)
+    def place(self):
+        self.device.go(*dbot.tray)
         time.sleep(5)
-        device.suck(False)
+        self.device.suck(False)
         time.sleep(1)
-        device.go(*tray[:2], 0.0)
+        self.device.go(*dbot.tray[:2], z=0.0)
         time.sleep(5)
 
-    time.sleep(0.5)
-    device.speed(10)
+    def goCalib(self,x ,y):
+        self.device.go(x,y,0)
+        time.sleep(5)
+    
+    def setSpeed(self, speed):
+        self.device.speed(speed)
+    
+    def closeDevice(self):
+        self.device.close()
+        time.sleep(0.5)
     #will have to 
     """device.go(*hide)"""
 
     #time.sleep(2)
     #device.go(250.0, 0.0, 50.0)
-    time.sleep(2)
-    device.close()
+
 
