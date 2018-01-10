4e58dee70f37bda631681b145dd83d44a0029817
sasinn98 <sasinn98@gmail.com>
2018-01-10 14:46:59 +0800

    arm code

---
+++ Arm/sample.py
@@ -0,0 +1,25 @@
+import time
+from glob import glob
+
+from pydobot import Dobot
+import serial.tools.list_ports
+
+
+ports = list(serial.tools.list_ports.comports())
+if len(ports) == 0:
+    print('no port found for Dobot Magician')
+    exit(1)
+	
+for p in ports:
+	print(p.device)
+
+print(ports[0].device)
+device = Dobot(port=ports[0].device)
+
+time.sleep(0.5)
+device.speed(10)
+device.go(135.0, 0.0, 0.0)
+#time.sleep(2)
+#device.go(250.0, 0.0, 50.0)
+time.sleep(2)
+device.close()
