ea294ef421d59fa24419f14374ed52f49bbd6b11
ChollSong <chollsong212@gmail.com>
2018-01-10 19:24:15 +0800

    Without egde version

--- Arm/CoordTransformer.py
+++ Arm/CoordTransformer.py
@@ -13,14 +13,16 @@ class CoordTransformer:
         cenX = center Pixel x coordinate
         cenY = center Pixel y coordinate
     """
-
-    def __init__(self, mmPxRate, x1, x2, y1, y2, camX, camY, cenX, cenY):
+    def __init__(self, mmPxRate, camX, camY, cenX, cenY):
+    #def __init__(self, mmPxRate, x1, x2, y1, y2, camX, camY, cenX, cenY):
         #x+ up, x- down, y+ left, y- right
         self.mmPxRate = mmPxRate
+        """
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
+        """
         self.camX = camX
         self.camY = camY
         self.cenX = cenX
@@ -39,18 +41,20 @@ class CoordTransformer:
         realY1 = ((self.camX-50)/(self.mmPxRate))+self.cenY
         realX1 = ((self.camX-150)/(self.mmPxRate))+self.cenX
         realX2 = ((self.camX+150)/(self.mmPxRate))+self.cenX
+        """
         xScaling = ((realX1/self.x1)+(realX2/self.x2))/2.0
         yScaling = ((realY1/self.y1)+(realY2/self.y2))/2.0
-
+        """
         
 
-        xCoord = (-1*self.mmPxRate*yScaling*(yPx-self.cenY))+self.camX
-        yCoord = (-1*self.mmPxRate*xScaling*(xPx-self.cenX))+self.camY
-
+        #xCoord = (-1*self.mmPxRate*yScaling*(yPx-self.cenY))+self.camX
+        #yCoord = (-1*self.mmPxRate*xScaling*(xPx-self.cenX))+self.camY
+        yCoord = (-1*self.mmPxRate*(xPx-self.cenX))+self.camY
+        xCoord = (-1*self.mmPxRate*(yPx-self.cenY))+self.camX
         #find the undistorted terms using the values with the calibration points
         return (xCoord, yCoord)
 
     
 
-ct = CoordTransformer(1.05, 0,0,0,0, 50, 250, 320, 300)
-print(ct.getCoordinates((200,200)))
+#ct = CoordTransformer(1.05, 0,0,0,0, 50, 250, 320, 300)
+#print(ct.getCoordinates((200,200)))
